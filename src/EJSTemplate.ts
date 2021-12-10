5de3663c7ca06c6709dccd3fd192925ceb1da4d0
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-12-09 19:51:21 -0500

    adds juice css preprocessor

--- src/EJSTemplate.ts
+++ src/EJSTemplate.ts
@@ -1,10 +1,21 @@
 import {RenderContext} from "./RenderContext";
 import * as ejs from 'ejs';
+import juice from 'juice';
+
+export type EJSTemplateOptions = {
+    ejsOptions?: ejs.Options,
+    juiceOptions?: juice.Options
+}
 
 export class EJSTemplate<D> {
     constructor(public name: string, public template: string) { }
-    public async render(context: RenderContext<D>): Promise<Buffer> {
-        const result = await ejs.render(this.template, context, { async: true });
+    public async render(context: RenderContext<D>, opts?: EJSTemplateOptions): Promise<Buffer> {
+        const rawHtml = await ejs.render(this.template, context, {
+            ...(opts?.ejsOptions || {}),
+            async: true
+        });
+
+        const result = juice(rawHtml, opts?.juiceOptions);
 
         return Buffer.from(result, 'utf8');
     }
